cee615a81b5feefb9bdacd72a34651c439f9dc99
NotMainCode <106552134+NotMainCode@users.noreply.github.com>
2023-11-13 20:35:06 +0300

    Revert "Feature/combined PR"

--- main/api/v1/companies/urls.py
+++ main/api/v1/companies/urls.py
@@ -1,10 +1,3 @@
 """URLs configuration of the 'companies' endpoints of 'Api' app v1."""
-from django.urls import include, path
-from rest_framework.routers import DefaultRouter
 
-from api.v1.companies.views import CompanyViewSet
-
-router_v1 = DefaultRouter()
-router_v1.register("", CompanyViewSet, basename="companies")
-
-urlpatterns = [path("", include(router_v1.urls))]
+urlpatterns = []
